0e0495e30f0443befc03d042603fbe773608bfa0
TheGatesOfParadise <109245867+TheGatesOfParadise@users.noreply.github.com>
2023-01-08 14:47:22 -0500

    Initial Commit

---
+++ GamifiedToDo/GamifiedToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamifiedToDoApp.swift
+//  GamifiedToDo
+//
+//  Created by Mom macbook air on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GamifiedToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
