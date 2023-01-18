c92d6b1c2fb71e2979259ed6ef9e0c17fa775796
TheGatesOfParadise <109245867+TheGatesOfParadise@users.noreply.github.com>
2023-01-17 23:18:42 -0500

    cleanup commented code

--- GamifiedToDo/ToDoView.swift
+++ GamifiedToDo/ToDoView.swift
@@ -51,11 +51,7 @@ struct ToDoView: View {
 
 struct HeaderView: View {
     @EnvironmentObject var userModel : UserModel
-  //  @State var emptyTodo =  Todo.getAnEmptyToDo()
-    
- //   @State var hiddenFlag:Bool = false
-    //@Binding var toDo: Todo
-    
+
     var body: some View {
         HStack {
             AvatarView(avatar: userModel.user.avatar)
@@ -78,11 +74,9 @@ struct HeaderView: View {
                     .font(.system(.largeTitle))
                     .frame(width: 60, height: 60)
                     .foregroundColor(Color.white)
-                //.padding(.trailing, 7)
             })
             .background( Color.yellow.opacity(0.6))
             .cornerRadius(30)
-            //.padding(.bottom, 10)
             .shadow(color: Color.black.opacity(0.3),
                     radius: 3,
                     x: 3,
@@ -225,148 +219,3 @@ struct ToDoView_Previews: PreviewProvider {
         ToDoView().environmentObject(UserModel(user: User.getASampleUser(), rules: Rules()))
     }
 }
-
-/*
-//beging of new add button
-struct AddButton: View {
-    @State var emptyTodo =  Todo.getAnEmptyToDo()
-    var body: some View {
-        VStack{
-            //Spacer()
-            HStack {
-                Spacer()
-                
-                NavigationLink(destination: {
-                    ToDoDetailsView(toDo: $emptyTodo, type: .New)
-                }, label: {
-                    Text("+")
-                        .font(.system(.largeTitle))
-                        .frame(width: 60, height: 60)
-                        .foregroundColor(Color.white)
-                    //.padding(.trailing, 7)
-                })
-                .background( Color.yellow.opacity(0.6))
-                .cornerRadius(30)
-                //.padding(.bottom, 10)
-                .shadow(color: Color.black.opacity(0.3),
-                        radius: 3,
-                        x: 3,
-                        y: 3)
-            }
-            Spacer()
-            Spacer()
-        }
-    }
-}
-
-
-//end of new add button
-*/
-
-/*
-//////////////////////////////////////////////////////////////////////////////////////////
-//beging of floating add  button
-struct AddButtonPopup: View {
-    @State var isPopup: Bool = false
-    var body: some View {
-        
-        ///this portion display a blurred view when PopupView is presented
-        ///the idea comes from youttube video:
-        ///https://www.youtube.com/watch?v=Ik8WmcERros&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=63
-        GeometryReader { proxy in
-            /* BlurView(style: viewMode == .original ? .systemThinMaterialLight : .systemThinMaterialDark )
-             .opacity(isPopup ? 1 : 0) */
-            BlurView(style: .systemThinMaterialDark)
-                .opacity(isPopup ? 1 : 0)
-        }
-        
-        ///this portion displays a popup view when user clicks on "+" button
-        PopupView(isPopup: $isPopup)
-        
-        ///display a "+" button if there is no popup, or
-        ///dispaly a "x" button when popup is presented
-        floatButton(isPopup: $isPopup,
-                    textSign: isPopup ? "x" : "+")
-    }
-}
-
-
-
-///
-///Blurs entries screen
-///
-///This comes from a yourtube video:
-///https://www.youtube.com/watch?v=Ik8WmcERros&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=63
-///
-struct BlurView: UIViewRepresentable {
-    let style: UIBlurEffect.Style
-    
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        //do nothing
-    }
-    
-    func makeUIView(context: Context) ->  UIVisualEffectView {
-        let view = UIVisualEffectView(effect:  UIBlurEffect(style: style))
-        return view
-    }
-}
-
-///
-///It shows a popup view which allow users to enter new to-do item
-///
-struct PopupView: View {
-    @Binding var isPopup: Bool
-    
-    var body: some View {
-        ZStack{
-            VStack{
-                Text("this is a popup view")
-            }
-        }
-        .opacity(isPopup ? 1 : 0)
-        .frame(width: 300, height: 400)
-        
-    }
-}
-
-
-///
-///float button at the bottom right corner
-///
-///it's referenced from this article:
-///https://medium.com/programming-with-swift/create-a-floating-action-button-with-swiftui-4d05dcddc365
-///
-struct floatButton: View {
-    @Binding var isPopup: Bool
-    var textSign: String
-    
-    var body: some View {
-        VStack{
-            //Spacer()
-            HStack {
-                Spacer()
-                Button(action: {
-                    isPopup.toggle()
-                }, label: {
-                    Text(textSign)
-                        .font(.system(.largeTitle))
-                        .frame(width: 60, height: 60)
-                        .foregroundColor(Color.white)
-                        //.padding(.trailing, 7)
-                })
-                .background( Color.yellow.opacity(0.6))
-                .cornerRadius(30)
-                //.padding(.bottom, 10)
-                .shadow(color: Color.black.opacity(0.3),
-                        radius: 3,
-                        x: 3,
-                        y: 3)
-            }
-            Spacer()
-            Spacer()
-        }
-    }
-}
-//end of floating add button
-//////////////////////////////////////////////////////////////////////////////////////////
-*/
